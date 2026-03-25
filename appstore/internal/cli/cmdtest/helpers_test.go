9f1dcc2d5d2f1ab587795765483d15bb3adb2be2
Abdullah AlRashoudi <abdullahalrashoudi@Abdullah4AIs-Mac-mini.local>
2026-03-25 10:00:21 +0300

    sync: upstream updates 2026-03-25 [appstore]

--- appstore/internal/cli/cmdtest/helpers_test.go
+++ appstore/internal/cli/cmdtest/helpers_test.go
@@ -4,10 +4,20 @@ import (
 	"github.com/peterbourgon/ff/v3/ffcli"
 
 	cmd "github.com/Abdullah4AI/apple-developer-toolkit/appstore/cmd"
+	"github.com/Abdullah4AI/apple-developer-toolkit/appstore/internal/asc"
+	"github.com/Abdullah4AI/apple-developer-toolkit/appstore/internal/auth"
 	"github.com/Abdullah4AI/apple-developer-toolkit/appstore/internal/cli/shared"
 )
 
+func resetCmdtestState() {
+	asc.ResetConfigCacheForTest()
+	auth.ResetInvalidBypassKeychainWarningsForTest()
+	shared.ResetDefaultOutputFormat()
+	shared.ResetTierCacheForTest()
+}
+
 func RootCommand(version string) *ffcli.Command {
+	resetCmdtestState()
 	return cmd.RootCommand(version)
 }
 
